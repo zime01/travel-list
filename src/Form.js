f995d974d43dc255e6d8d0eec8a543c049781480
Hazim Zimić <148982820+zime01@users.noreply.github.com>
2024-10-15 11:26:38 +0200

    Add files via upload

---
+++ src/Form.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+export default function Form({ onAddItems }) {
+  const [quantity, setQuantity] = useState(1);
+
+  const [description, setDescription] = useState("test");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    onAddItems(newItem);
+    setDescription("");
+    setQuantity("");
+  }
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip</h3>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          setQuantity(e.target.value);
+        }}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        value={description}
+        placeholder="Item..."
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
